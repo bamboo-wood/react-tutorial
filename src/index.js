3bace14d772545fb4719887313bd773e09e6adcd
bamboo-wood <64763469+bamboo-wood@users.noreply.github.com>
2022-06-20 19:21:05 +0900

    add App.jsx

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
-const App = () => {
-  return (
-    <React.Fragment>
-      <h1>hello</h1>
-      <p>I'm bamboo-wood</p>
-    </React.Fragment>
-  );
-};
+import App from "./App";
 
 const container = document.getElementById("root");
 const root = createRoot(container);

b9a0c9cdf7256ad963384008df65fa19393ec5ba
bamboo-wood <64763469+bamboo-wood@users.noreply.github.com>
2022-06-20 19:21:20 +0900

    add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1>hello</h1>
+      <p>I'm bamboo-wood</p>
+    </React.Fragment>
+  );
+};
+
+// 他のファイルでも参照できるようにする
+export default App;

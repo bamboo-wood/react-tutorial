a39ca20ae65a22ac60f556c43ab782ef0e23d6f7
bamboo-wood <64763469+bamboo-wood@users.noreply.github.com>
2022-06-20 19:17:52 +0900

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App";
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1>hello</h1>
+      <p>I'm bamboo-wood</p>
+    </React.Fragment>
+  );
+};
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
